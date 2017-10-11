48862668ed29ea9ab1cff14ac3f3db342f799ca4
sharad joe <sharadjoe48@gmail.com>
2017-10-11 15:45:05 +0530

    added the switch case

---
+++ prodcon.c
@@ -0,0 +1,20 @@
+#include<conio.h>
+#include<stdlib.h>
+void main()
+{
+    int mutex[100],a[100],size_buffer,size_customer;
+    clrscr();
+    void procedure();
+    void consumer();
+    printf("Enter the size of buffer:")
+    scanf("%d",&size_buffer);
+    printf("\nEnter the number of consumers:")
+    scanf("%d",size_customer);
+    while(1)
+    {
+        switch(choice)
+        {
+            case 1:
+        }
+    }
+}
