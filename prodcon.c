f495912430116cd92e92d8733119590dcb4af4d0
sharad joe <sharadjoe48@gmail.com>
2017-10-13 10:37:10 +0530

    removing bugs phase 1

--- prodcon.c
+++ prodcon.c
@@ -8,7 +8,7 @@ void main()
     printf("Enter the size of buffer:")
     scanf("%d",&size_buffer);
     printf("\nEnter the number of consumers:")
-    scanf("%d",size_customer);
+    scanf("%d",&size_customer);
     while(1)
     {
         switch(choice)
