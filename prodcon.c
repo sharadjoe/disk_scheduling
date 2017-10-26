6f0fed47a78c5af0524350e947aa89256a6901f9
Sharad Joe <sharadjoe48@gmail.com>
2017-10-26 18:14:03 +0530

    Took care of the unwanted display problem

--- prodcon.c
+++ prodcon.c
@@ -54,8 +54,7 @@ int main()
     printf("\nDo you want to continue(0/1):");
     scanf("%d",&condition);
     }
-    for(i=0;i<size_consumer;i++)
-        printf("%d",mutex[i]);
+    
 }
 
 int producer(int x)
